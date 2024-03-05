042a179b1aa40f8a4190acbb4396726552502c71
Hasban-Fardani <hasbanfardani77@gmail.com>
2024-03-05 12:48:12 +0700

    feat: auth & admin layout, add mazer template

---
+++ resources/views/auth/register.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.auth')
+
+@section('title', 'Login')
+
+@section('content')
+    <div class="auth-logo">
+        <a href="/"><img src="./assets/compiled/svg/logo.svg" alt="Logo"></a>
+    </div>
+    <h1 class="auth-title">Sign Up</h1>
+    <p class="auth-subtitle mb-5">Input your data to register to our website.</p>
+
+    <form action="{{ route('register') }}" method="POST">
+        <div class="form-group position-relative has-icon-left mb-4">
+            <input type="text" class="form-control form-control-xl" placeholder="Email">
+            <div class="form-control-icon">
+                <i class="bi bi-envelope"></i>
+            </div>
+        </div>
+        <div class="form-group position-relative has-icon-left mb-4">
+            <input type="text" class="form-control form-control-xl" placeholder="Username">
+            <div class="form-control-icon">
+                <i class="bi bi-person"></i>
+            </div>
+        </div>
+        <div class="form-group position-relative has-icon-left mb-4">
+            <input type="password" class="form-control form-control-xl" placeholder="Password">
+            <div class="form-control-icon">
+                <i class="bi bi-shield-lock"></i>
+            </div>
+        </div>
+        <div class="form-group position-relative has-icon-left mb-4">
+            <input type="password" class="form-control form-control-xl" placeholder="Confirm Password">
+            <div class="form-control-icon">
+                <i class="bi bi-shield-lock"></i>
+            </div>
+        </div>
+        <button class="btn btn-primary btn-block btn-lg shadow-lg mt-5">Sign Up</button>
+    </form>
+    <div class="text-center mt-5 text-lg fs-4">
+        <p class='text-gray-600'>Already have an account? <a href="auth-login.html" class="font-bold">Log
+                in</a>.</p>
+    </div>
+@endsection
